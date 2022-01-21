8060c733c4c8bf9ea699855b27f23a5905948169
Abhijeet Parida <abhijeet.parida@tum.de>
2022-01-21 20:40:45 +0100

    intro to rust

---
+++ Chap1/main.rs
@@ -0,0 +1,16 @@
+// This is a comment, and is ignored by the compiler
+/* Block comment
+ *line 1
+ *line 2
+ *These individual starts are for style only
+*/
+
+/// Generate library docs for the following item.
+//! Generate library docs for the enclosing item.!//
+fn main() {
+    // Statements here are executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+    println!("I'm a Rustacean!");
+}
