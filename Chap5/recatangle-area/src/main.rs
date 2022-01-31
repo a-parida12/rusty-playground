47ca10e20dbabbf9521e6a127fa11b210e465ae6
Abhijeet Parida <abhijeet.parida@tum.de>
2022-01-31 21:45:47 +0100

    struct method eg

--- Chap5/recatangle-area/src/main.rs
+++ Chap5/recatangle-area/src/main.rs
@@ -3,6 +3,12 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle{
+    fn area_rectangle(&self)->u32{
+        self.width*self.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
@@ -12,6 +18,11 @@ fn main() {
     println!(
         "The area of the rectangle is {} square pixels.",
         area(&rect1)
+    );
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area_rectangle()
     );
 }
 
